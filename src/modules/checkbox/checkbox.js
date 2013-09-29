68e31c51ccd6807bbd38361f62be1c44565e7c5d
Caitlin Potter <snowball@defpixel.com>
2013-09-29 13:07:17 -0400

    docs(checkbox): Generate docs for checkbox/slider/toggle

--- src/modules/checkbox/checkbox.js
+++ src/modules/checkbox/checkbox.js
@@ -2,6 +2,83 @@
 
 var module = angular.module('ui.semantic.checkbox', []);
 
+/**
+ * @ngdoc directive
+ * @name ui.semantic.checkbox.directive:checkbox
+ * @restrict EA
+ * @element ANY
+ * @scope
+ *
+ *
+ * @param onEnable Callback after a checkbox is enabled.
+ * @param onDisable Callback after a checkbox is disabled.
+ * @param onChange Callback after a checkbox is either disabled or enabled.
+ *
+ * @description
+ *
+ * A checkbox visually indicates the status of a user's selection
+ *
+ * **Callbacks**
+ * There are 3 callbacks used by the accordion module:
+ *   - onDisable()
+ *   - onEnable()
+ *   - onChange(checked)
+ *
+ * Each callback will optionally evaluate an AngularJS expression. onChange provides access to the
+ * current checkbox state.
+ */
+
+/**
+ * @ngdoc directive
+ * @name ui.semantic.checkbox.directive:toggle
+ * @restrict EA
+ * @element ANY
+ * @scope
+ *
+ *
+ * @param onEnable Callback after a checkbox is enabled.
+ * @param onDisable Callback after a checkbox is disabled.
+ * @param onChange Callback after a checkbox is either disabled or enabled.
+ *
+ * @description
+ *
+ * A checkbox visually indicates the status of a user's selection
+ *
+ * **Callbacks**
+ * There are 3 callbacks used by the accordion module:
+ *   - onDisable()
+ *   - onEnable()
+ *   - onChange(checked)
+ *
+ * Each callback will optionally evaluate an AngularJS expression. onChange provides access to the
+ * current checkbox state.
+ */
+
+/**
+ * @ngdoc directive
+ * @name ui.semantic.checkbox.directive:slider
+ * @restrict EA
+ * @element ANY
+ * @scope
+ *
+ *
+ * @param onEnable Callback after a checkbox is enabled.
+ * @param onDisable Callback after a checkbox is disabled.
+ * @param onChange Callback after a checkbox is either disabled or enabled.
+ *
+ * @description
+ *
+ * A checkbox visually indicates the status of a user's selection
+ *
+ * **Callbacks**
+ * There are 3 callbacks used by the accordion module:
+ *   - onDisable()
+ *   - onEnable()
+ *   - onChange(checked)
+ *
+ * Each callback will optionally evaluate an AngularJS expression. onChange provides access to the
+ * current checkbox state.
+ */
 angular.forEach(['checkbox', 'slider', 'toggle'], function(name, _) {
   var needCheckbox = name !== 'checkbox' ? 'checkbox ' : '',
       inputType = name !== 'radio' ? 'checkbox' : 'radio';
